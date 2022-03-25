ab03e2229ff0cd2b42694fc26227d8c8847dee45
Marlene Navarro <88111665+meaguilar@users.noreply.github.com>
2022-03-24 19:02:44 -0600

    Add files via upload

---
+++ CP2/cp2/cp2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace cp2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Escribe tu programa, pero antes debes analizar el diagrama de clases en la guia
+             * https://meaguilar.github.io/POO/Evaluaciones/cp2/#0
+             * 
+             * Resultado esperado imprimir en pantalla los datos del empleado,
+             * su deducción salarial, y su salario aplicada las deducciones y  si es apto para el retiro según su edad y género. 
+             */
+        }
+    }
+}
